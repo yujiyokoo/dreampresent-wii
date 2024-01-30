cc83b6015c138a8e8043e766223518fb0f4ea60c
Yuji Yokoo <yujiyokoo@gmail.com>
2024-01-30 19:33:46 +0900

    Hello world with GRRLIB

--- src/main.c
+++ src/main.c
@@ -1,69 +1,37 @@
+#include <grrlib.h>
+
+#include <wiiuse/wpad.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <gccore.h>
-#include <wiiuse/wpad.h>
 #include <mruby.h>
 #include <mruby/irep.h>
 #include <mruby/string.h>
 #include <mruby/array.h>
 #include "mrbwii.h"
+#include "font_png.h"
 
-static u32 *xfb = NULL;
-static GXRModeObj *rmode = NULL;
+#define GRRLIB_WHITE   0xFFFFFFFF
+#define GRRLIB_BLACK   0x3333FFFF
 
 extern const uint8_t program[];
 
-//---------------------------------------------------------------------------------
+GRRLIB_texImg *tex_font;
 int main(int argc, char **argv) {
-  //---------------------------------------------------------------------------------
-  // Initialise the video system
-  VIDEO_Init();
+  GRRLIB_Init();
+  tex_font = GRRLIB_LoadTexture(font_png);
+  GRRLIB_InitTileSet(tex_font, 8, 16, 0);
 
-  // This function initialises the attached controllers
-  //WPAD_Init();
-  PAD_Init();
+  // pad initialisation
+  WPAD_Init();
+  WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
 
-  // Obtain the preferred video mode from the system
-  // This will correspond to the settings in the Wii menu
-  rmode = VIDEO_GetPreferredMode(NULL);
-
-  // Allocate memory for the display in the uncached region
-  xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-
-  // Initialise the console, required for printf
-  console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-
-  // Set up the video registers with the chosen mode
-  VIDEO_Configure(rmode);
-
-  // Tell the video hardware where our display memory is
-  VIDEO_SetNextFramebuffer(xfb);
-
-  // Make the display visible
-  VIDEO_SetBlack(FALSE);
-
-  // Flush the video register changes to the hardware
-  VIDEO_Flush();
-
-  // Wait for Video setup to complete
-  VIDEO_WaitVSync();
-  if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-
-
-  // The console understands VT terminal escape codes
-  // This positions the cursor on row 2, column 0
-  // we can use variables for this with format codes too
-  // e.g. printf ("\x1b[%d;%dH", row, column );
-  printf("\x1b[2;0H");
-
-  mrb_state *mrb = mrb_open();
-  if (!mrb) { return 1; }
-  struct RClass *dwii_module = mrb_define_module(mrb, "DreamPresentWii");
-  define_module_functions(mrb, dwii_module);
-
-  mrb_load_irep(mrb, program);
   while(1) {
+    GRRLIB_FillScreen(GRRLIB_BLACK);
+    GRRLIB_Printf(5, 25, tex_font, GRRLIB_WHITE, 1, "%s", "Hello, world");
+    GRRLIB_Render();
+    VIDEO_WaitVSync();
   }
 
-	return 0;
+  return 0;
 }
