031ae40b74530bfe80b2a8edaa1872febe37bbfc
Yuji Yokoo <yujiyokoo@gmail.com>
2024-03-16 18:29:01 +0900

    Define puts and p that work in Dolphin console

--- src/start.rb
+++ src/start.rb
@@ -1,5 +1,23 @@
+# define a new version of puts and p so they show up in terminal
+module Kernel
+  alias_method :original_puts, :puts
+  def puts(*args)
+    original_puts(*args)
+    original_puts "\r"
+  end
+
+  alias_method :original_p, :p
+  def p(*args)
+    original_p(*args)
+    original_puts "\r"
+  end
+end
+
 begin
   # DreamPresentWii.print_msg 'Starting presentation.'
+  DreamPresentWii.print_msg "test message from print_msg"
+  puts "test message from puts"
+  p "test message from p"
   Dreampresent.new(DcKosRb.new(DreamPresentWii)).start
 rescue => ex
   # Note backtrace is only available when you pass -g to mrbc
