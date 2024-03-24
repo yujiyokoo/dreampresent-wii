b54e5752d0cec00cdff735a424a67c5696f06a3d
Yuji Yokoo <yujiyokoo@gmail.com>
2024-03-24 14:50:15 +0900

    Fix colour support for drawing horizontal lines

--- src/mrbwii.c
+++ src/mrbwii.c
@@ -89,33 +89,15 @@ static mrb_value dpad_left(mrb_state *mrb, mrb_value self) {
   return mrb_bool_value(state & PAD_BUTTON_LEFT);
 }
 
-static u32 CvtRGB (u8 r1, u8 g1, u8 b1, u8 r2, u8 g2, u8 b2)
-{
-	int y1, cb1, cr1, y2, cb2, cr2, cb, cr;
-
-	y1 = (299 * r1 + 587 * g1 + 114 * b1) / 1000;
-	cb1 = (-16874 * r1 - 33126 * g1 + 50000 * b1 + 12800000) / 100000;
-	cr1 = (50000 * r1 - 41869 * g1 - 8131 * b1 + 12800000) / 100000;
-
-	y2 = (299 * r2 + 587 * g2 + 114 * b2) / 1000;
-	cb2 = (-16874 * r2 - 33126 * g2 + 50000 * b2 + 12800000) / 100000;
-	cr2 = (50000 * r2 - 41869 * g2 - 8131 * b2 + 12800000) / 100000;
-
-	cb = (cb1 + cb2) >> 1;
-	cr = (cr1 + cr2) >> 1;
-
-	return (y1 << 24) | (cb << 16) | (y2 << 8) | cr;
-}
-
-static u32 PACK_PIXEL(int r, int g, int b) {
-  return CvtRGB(r, g, b, r, g, b);
+static u32 PACK_PIXEL(u8 r, u8 g, u8 b, u8 a) {
+  return (r << 24) | (g << 16) | (b << 8) | a;
 }
 
 static mrb_value draw_horizontal_line(mrb_state *mrb, mrb_value self) {
   mrb_int x, y, len, r, g, b;
   mrb_get_args(mrb, "iiiiii", &x, &y, &len, &r, &g, &b);
 
-  u32 rgb = PACK_PIXEL(r, g, b);
+  u32 rgb = PACK_PIXEL(r, g, b, 255);
 
   GRRLIB_Line(x, y, x + len, y, rgb);
 
@@ -126,7 +108,7 @@ static mrb_value draw_vertical_line(mrb_state *mrb, mrb_value self) {
   mrb_int x, y, len, r, g, b;
   mrb_get_args(mrb, "iiiiii", &x, &y, &len, &r, &g, &b);
 
-  u32 rgb = PACK_PIXEL(r, g, b);
+  u32 rgb = PACK_PIXEL(r, g, b, 255);
 
   GRRLIB_Line(x, y, x, y + len, rgb);
 
